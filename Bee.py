824891e651cba4ba3fafb3b40cd7af3c3168cc8a
Sander van oostveen <sander.vanoostveen@student.uva.nl>
2020-01-14 21:19:33 +0100

    I have commited warcrimes against beekind

--- Bee.py
+++ Bee.py
@@ -8,54 +8,33 @@ class Bee:
         self.hive_location = hive_location
         self.pos_x = hive_location[0]
         self.pos_y = hive_location[1]
-        self.load_pollen = 0
-        self.load_nectar = 0
+        self.food_cargo = 0
+        self.age = 0
 
         self.food_location = []
         self.food_priority = 0
-
-    def gather():
-        pass
-
-    def gather_food():
-        pass
-
-    def go_home():
-        pass
-
-    def go_to_food():
-        pass
-
-    def dance():
-        pass
-
-
-class Scout(Bee):
-    def __init__(self, hive_location):
-        super().__init__(hive_location)
         self.hive_distance = 0
 
-    def update(self):
+    def update(self, grid):
+        self.age += 1
         if self.employed:
-            self.gather()
+            pass
+        else:
+            self.search(grid)
+
+        return self.still_alive()
 
 
     def search(self, grid):
+        self.pos_x = random.randint(0,2) - 1
+        self.pos_y = random.randint(0,2) - 1
+
         tile_value = grid[self.pos_x][self.pos_y]
         if tile_value > FOOD_BIAS:
             self.employed = True
-        else:
-            pass
+            self.food_location[self.pos_x, self.pos_y]
+            self.food_priority = tile_value
 
 
     def check_block():
         pass
-
-
-class Employee(Bee):
-    def __init__(self, hive_location):
-        super().__init__(hive_location)
-
-    def update(self):
-        if self.employed:
-            self.gather()
--- Hive.py
+++ Hive.py
@@ -1,22 +1,21 @@
+import Bee
+
 FORAGERS_TO_POPULATION = 1/3
 SCOUTS_TO_EMPLOYEES = 1/10
 
 #Maintain population and food levels, spawn new bees as needed
 class Hive:
-    def __init__(self, pos_x, pos_y, start_population, start_pollen, start_nectar, growth_rate, consumption_rate):
+    def __init__(self, pos_x, pos_y, start_population, start_food, growth_rate, consumption_rate):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.population =  start_population
         self.growth_rate = growth_rate
         self.consumption_rate = consumption_rate
-        self.pollen = start_pollen
-        self.nectar = start_nectar
-        self.food_level = 1.1
+        self.food_level = start_food
 
         num_foragers = int(FORAGERS_TO_POPULATION * population)
         self.scouts = [Scout([pos_x,pos_y]) for _ in range(int(SCOUTS_TO_EMPLOYEES * num_foragers))]
-        self.employees = [Employee([pos_x,pos_y]) for _ in range(num_foragers - len(self.scouts))]
-        self.employed = []
+        self.num_employees = num_foragers - len(self.scouts)
 
 
     # Determine food level based on amount of pollen, nectar and growth rate.
@@ -24,20 +23,24 @@ class Hive:
     def update_food_level(self):
         pass
 
-    #Update food levels and bee populations
-    def update(self):
-        self.update_food_level()
-        self.population = self.population * food_level
+    def update_population(self):
+        self.population = self.population * self.food_level
 
         num_foragers = int(FORAGERS_TO_POPULATION * self.population)
-        num_scouts = int(foragers * SCOUTS_TO_EMPLOYEES)
-        num_employees = num_foragers - num_scouts
+        num_scouts = int(num_foragers * SCOUTS_TO_EMPLOYEES)
+        self.num_employees = num_foragers - num_scouts
 
         new_scouts = num_scouts - len(self.scouts)
-        new_employees = num_employees - len(self.employees)
 
         for _ in range(new_scouts):
             self.scouts.append(Scout([self.pos_x, self.pos_y]))
 
-        for _ in range(new_employees):
-            self.scouts.append(Employee([self.pos_x, self.pos_y]))
+    def update_bees(self, grid):
+        updated_scouts = []
+        for scouts in self.scouts:
+            scout.update()
+
+    #Update food levels and bee populations
+    def update(self):
+        self.update_food_level()
+        self.update_population()
