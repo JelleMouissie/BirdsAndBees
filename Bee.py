bd95e849df88aaf8fec8ec557f1038ef183bbbf4
Sander van oostveen <sander.vanoostveen@student.uva.nl>
2020-01-15 00:28:21 +0100

    Improved Bee/Hive functionality

--- Bee.py
+++ Bee.py
@@ -1,40 +1,50 @@
 import random
+import math
 
 FOOD_BIAS = 0.75
 
 class Bee:
+    def __init__(self):
+        self.age = 0
+        self.cargo = 0
+
+    #Determine if bee dies, return False. Else if bee lives, increase age and return True
+    def update_age(self):
+        if self.age > 8:      #TODO: Implement actual death condition based on probability                             !!!
+            return False
+        else:
+            self.age += 1
+            return True
+
+
+class Scout(Bee):
     def __init__(self, hive_location):
-        self.employed = False
+        super().__init__()
         self.hive_location = hive_location
         self.pos_x = hive_location[0]
         self.pos_y = hive_location[1]
-        self.food_cargo = 0
-        self.age = 0
 
         self.food_location = []
-        self.food_priority = 0
+        self.food_value = 0
         self.hive_distance = 0
 
-    def update(self, grid):
-        self.age += 1
-        if self.employed:
-            pass
-        else:
-            self.search(grid)
 
-        return self.still_alive()
+    #Update scout for 1 simulation step. If employed, do nothing (Handled in Hive.py)
+    def update(self, grid):
+        if self.food_value > FOOD_BIAS:
+            return True
+        return self.search(grid)
 
 
     def search(self, grid):
         self.pos_x = random.randint(0,2) - 1
         self.pos_y = random.randint(0,2) - 1
 
-        tile_value = grid[self.pos_x][self.pos_y]
-        if tile_value > FOOD_BIAS:
-            self.employed = True
+        tile_value = grid[self.pos_x][self.pos_y].value   #TODO: Figure out how to check ground's tile_value                    !!!
+        if tile_value > FOOD_BIAS:                        #TODO: Determine when food source is good enough.             !!!
             self.food_location[self.pos_x, self.pos_y]
-            self.food_priority = tile_value
+            self.food_value = tile_value
+            self.hive_distance = math.sqrt(math.pow((self.pos_x - self.hive_location[0]), 2) + math.pow((self.pos_y - self.hive_location[1]))
 
-
-    def check_block():
-        pass
+            return True
+        return False
--- Hive.py
+++ Hive.py
@@ -3,6 +3,8 @@ import Bee
 FORAGERS_TO_POPULATION = 1/3
 SCOUTS_TO_EMPLOYEES = 1/10
 
+PRIORITY_BIAS = 2     #TODO: Decide Bias in determining priority of found food source.
+
 #Maintain population and food levels, spawn new bees as needed
 class Hive:
     def __init__(self, pos_x, pos_y, start_population, start_food, growth_rate, consumption_rate):
@@ -15,32 +17,65 @@ class Hive:
 
         num_foragers = int(FORAGERS_TO_POPULATION * population)
         self.scouts = [Scout([pos_x,pos_y]) for _ in range(int(SCOUTS_TO_EMPLOYEES * num_foragers))]
-        self.num_employees = num_foragers - len(self.scouts)
+        self.employees = [Bee() for _ in range(num_foragers - len(self.scouts))]
+        self.gather_group_id = [0]
 
 
-    # Determine food level based on amount of pollen, nectar and growth rate.
-    # Also adjust amount of food left aftwards based on consumption rate of food.
+    #Do stuff with food?
     def update_food_level(self):
-        pass
+        pass       #TODO: Implement this                                                                                                   !!!!
 
+
+    #Increase population and determine new amount of foragers and scouts.
     def update_population(self):
-        self.population = self.population * self.food_level
+        self.population = self.population * self.food_level #TODO: implement actual growth based on food supplies                          !!!!
 
         num_foragers = int(FORAGERS_TO_POPULATION * self.population)
         num_scouts = int(num_foragers * SCOUTS_TO_EMPLOYEES)
-        self.num_employees = num_foragers - num_scouts
+        num_employees = num_foragers - num_scouts
 
         new_scouts = num_scouts - len(self.scouts)
+        new_employes = num_employees - len(self.employees)
 
         for _ in range(new_scouts):
             self.scouts.append(Scout([self.pos_x, self.pos_y]))
 
+        for _ in range(new_employes):
+            self.scouts.append(Bee())
+
+
+    #Gather unemployed bees based on priority, and collectivly gather food
+    def gather_food(self, scout, grid):
+        priority = PRIORITY_BIAS * scout.food_value / scout.hive_distance    # Amount of employee bees assigned to food source
+        start_slice = self.gather_group_id[-1]
+        end_slice = start_slice + priority
+        self.gather_group_id.append(end_slice)
+
+        gather_group = self.employees[start_slice : end_slice]
+        self.food_level += grid[scout.food_location[0], scout.food_location[1]].gather_food(gather_group)    #TODO: gather food from grid location, update age of bees if location far away
+        scout.food_value = grid[scout.food_location[0], scout.food_location[1]].value                         #TODO: get tile's food value
+
+    #Kill Bees >:)
+    def update_bee_age(self):
+        alive_scouts = [scout for scout in self.scouts if scout.update_age()]
+        alive_employees = [employee for employee in self.employees if employee.update_age()]
+        num_dead = len(self.scouts) - len(alive_scouts) + len(self.employees) - len(alive_employees)
+
+        self.population -= num_dead
+        self.scouts = alive_scouts
+        self.employees = alive_employees
+
+    #First update scouts. If scout has a source, gather food with employee bees. Finally update age of all bees.
     def update_bees(self, grid):
         updated_scouts = []
-        for scouts in self.scouts:
-            scout.update()
+        for scout in self.scouts:
+            if scout.update():
+                self.gather_food(scout, grid)
+        self.update_bee_age()
 
-    #Update food levels and bee populations
-    def update(self):
+
+    #Update food levels, bee populations and perform bee actions
+    def update(self, grid):
         self.update_food_level()
         self.update_population()
+        self.update_bees(grid)
